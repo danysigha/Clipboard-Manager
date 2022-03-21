f29873f89b18f5faccf434e2790831d48d9db8d4
nko2005 <nko2005@nyu.edu>
2022-03-22 00:46:58 +0300

    copyProto is da program.Use the Sqllite file to create the database first

---
+++ Nawaf-Prototype/sqlLite.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+# conn = sqlite3.connect("imgString.db")
+# c= conn.cursor()
+# c.execute("""CREATE TABLE ImageStrings ( theImageString text)""")
+# 
+# conn.commit()
+# 
+# conn.close()
