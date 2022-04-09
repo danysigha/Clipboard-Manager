0753d49fc1dd96cbb982807534c9612a0f82963f
danysigha <nohonetdany@gmail.com>
2022-04-09 12:43:46 -0400

    The data access object class to the database.

---
+++ dao.py
@@ -0,0 +1,12 @@
+class dataaccessobject():
+    pass
+# decryption takes place here
+    # UI grabs the decryption key from user and pass to data access object
+    def grabAll(self):
+        conn = sqlite3.connect("ClipboardManager_DB.db")
+        c = conn.cursor
+        c.execute('SELECT * FROM card')
+        records = c.fectchall()
+        conn.commit()
+        conn.close()
+
