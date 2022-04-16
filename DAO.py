f42cae2a46cd5a6d9fecbdb36ccc7aa2e9515d96
tc0610 <83427595+tc0610@users.noreply.github.com>
2022-04-16 09:08:24 -0400

    Add files via upload

---
+++ DAO.py
@@ -0,0 +1,157 @@
+import hashlib
+import string
+import random
+
+
+class DAO:
+	
+	#create and attribute to hold EOF status 
+	def __init__(self):
+        #-------------------------------------------------
+        #Try to open database
+        #-------------------------------------------------
+        try:
+        	conn = sqlite3.connect('user1.db')
+			cursor = conn.cursor()
+
+		else:
+			print("Database unavailable for query")
+
+
+	#printing all the results
+	def print_table(table_content): 
+	    for i in range(len(table_content)):
+	        print("=========== " + str(table_content[i][0]) +  " ===========")
+	        for j in range(len(table_content[i])):
+	            print(table_content[i][j])
+
+	    print("===============================")
+
+    def test_db(userID, data_type = "all"):
+	    test_info_card = ["cardID", "cardContent", "cardCategory", "cardAddedDate", "cardModifiedDate", "folderID"]
+	    test_info_folder = ["folderID", "folderName", "userID"]
+	    test_info_user = ["userID", "currentCardID", "currentCardID", "shelfTime"]
+
+	    # print user table
+	    if (data_type == "user"):
+	        cursor.execute('SELECT * FROM user WHERE userID == ' + str(userID) + ';')
+	        table_list = cursor.fetchall()
+	        for i in range(len(table_list)):
+	            print("=============================")
+	            for j in range(len(table_list[i])):
+	                print(test_info_user[j] + ": " + str(table_list[i][j]))
+
+	    # print folder table
+	    elif (data_type == "folder"):
+	        cursor.execute('SELECT * FROM folder WHERE userID == ' + str(userID) + ';')
+	        table_list = cursor.fetchall()
+	        for i in range(len(table_list)):
+	            print("=============================")
+	            for j in range(len(table_list[i])):
+	                print(test_info_folder[j] + ": " + str(table_list[i][j]))
+
+	    # print card table
+	    elif (data_type == "card"):
+	        cursor.execute('SELECT cardID, cardContent, cardCategory, cardAddedDate, cardModifiedDate, card.folderID FROM card, folder WHERE folder.userID == ' + str(userID) + ' AND folder.folderID == card.folderID')
+	        table_list = cursor.fetchall()
+	        for i in range(len(table_list)):
+	            print("=============================")
+	            for j in range(len(table_list[i])):
+	                print(test_info_card[j] + ": " + str(table_list[i][j]))
+
+	            # result = cursor.fetchall()
+	            # print(result)
+	            # print("Belongs to user " + str(result[0][0]))
+
+	    else:
+	        print("========= USER =========")
+	        test_db(userID, "user")
+	        print("========= FOLDER =========")
+	        test_db(userID, "folder")
+	        print("========= CARD =========")
+	        test_db(userID, "card")
+
+	# to be implemented even more later -> copy card
+    def addCard(userID, content, category): 
+    	# find the user's default folder
+    	cursor.execute('SELECT defaultFolderID FROM user WHERE userID == ' + str(userID) + ";")
+    	defaultFolderID_loc = cursor.fetchall()[0][0]
+
+    	cursor.execute('INSERT INTO card(cardContent, cardCategory, cardAddedDate, cardModifiedDate, folderID) VALUES(?, ?, datetime("now", "localtime"), datetime("now", "localtime"), ?)', (content, category, defaultFolderID_loc))
+
+	    # get the currentCardID -> maybe can be done by the upper layer or dnt need it
+	    # cursor.execute('SELECT cardID FROM card, folder WHERE card.folderID == folder.folderID AND folder.userID == '+ str(userID) + ';')
+	    # result = cursor.fetchall()
+	    # cardIDs = list()
+	    # for i in range(len(result)):
+	    #     cardIDs.append(result[i][0])
+	    # cardIDs.sort() # so cardIDs[-1] would be the most recent card added (currentCardID)
+
+	    # cursor.execute('UPDATE user SET currentCardID = ' + str(cardIDs[-1]) + ' WHERE userID == ' + str(userID) + ';')
+
+	# paste function to be implemented later
+	def pasteCard(cardID):
+	    cursor.execute('SELECT cardContent FROM card WHERE cardID == ' + str(cardID) + ';')
+	    result = cursor.fetchall()
+	    return result[0][0]
+
+
+
+	# delete
+	def deleteCard(cardID):
+    	cursor.execute('DELETE FROM card WHERE cardID == ' + str(cardID) + ';')
+
+
+   	def automaticDelete_shelftime(userID):
+	    # this must be called once in a while by the main system for the system to check and delete
+	    cursor.execute('SELECT userID, shelfTime FROM user WHERE userID == ' + str(userID))
+	    shelftime_months = cursor.fetchall()[0][1]
+	    # print(shelftime_months)
+	    cursor.execute('DELETE FROM card WHERE cardAddedDate <= datetime(\'now\', \'localtime\', \'-' + str(shelftime_months) +' months\');')
+
+	# search
+	# def showCard(userID, searchType, searchContent):
+    	# cursor.execute('SELECT * FROM card, folder WHERE userID == ' + str(userID) + ' AND ' + str(searchType) + '')
+
+	# sorting
+	def sortCard(folderID, sortType):
+	    # the folderID is included because we usually will sort within the folder that the user is looking at currently.
+	    # please enter the folder that the user is currently taking a look at
+	    cursor.execute('SELECT * FROM card WHERE folderID == ' + str(folderID) + ' ORDER BY ' + str(sortType))
+	    return cursor.fetchall()
+
+	# change shelf time
+	def changeShelfTIme(userID, newShelfTime):
+	    cursor.execute('UPDATE user SET shelfTime = ' + str(newShelfTime) + ' WHERE userID == ' + str(userID) + ';')
+
+	# set password?
+
+	# hide card
+
+	# favorite function
+
+	# create folder (HERE)
+	def createFolder(userID, folderName):
+	    cursor.execute("INSERT INTO folder(folderName, userID) VALUES(?, ?)", (folderName, userID))
+
+	# delete folder
+	def deleteFolder(userID, folderID):
+	    cursor.execute('DELETE FROM folder WHERE folderID == ' + str(folderID) + ';')
+
+	# update folder
+	def updateFolderID_all(userID, currentFolderID, newFolderID):
+	    # this function changes the folderID inside the card entity, not the folder itself
+	    # this function changes all the folderIDs of the matching cards
+	    # can be used to change entire card deck.
+	    cursor.execute('UPDATE card SET folderID = ' + str(newFolderID) + ' WHERE folderID == ' + str(currentFolderID) + ';')
+
+	def updateFolderID_indiv(userID, cardID, newFolderID):
+	    # this function changes the folderID inside the card entity, but solely one card.
+	    cursor.execute('UPDATE card SET folderID = ' + str(newFolderID) + ' WHERE cardID == ' + str(cardID) +';')
+
+	# rename folder
+	def updateFolder_Name(userID, currentFolderName, newFolderName): # because we do not know what the folderID is on point, let the program do it
+	    # find currentFolderID
+	    cursor.execute('UPDATE card SET folderName = ' + str(newFolderName) + ' WHERE folderName == ' + str(currentFolderName))
+
+	# unlock password
