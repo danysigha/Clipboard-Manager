61e2476d82d0f3ae4a2534b099193c5b5591f623
danysigha <nohonetdany@gmail.com>
2022-04-17 15:06:46 -0400

    Init method and initialize method added to DAO.

--- dao.py
+++ dao.py
@@ -1,15 +1,30 @@
 import sqlite3
 import clipboardManager_DB as db
 
-class dataaccessobject():
-    pass
+class DataAccessObject():
+    def __init__(self):
+        pass
 # decryption takes place here
     # UI grabs the decryption key from user and pass to data access object
-    def grabAll(self):
+
+
+    def initializeUI(self):
         conn = sqlite3.connect("ClipboardManager_DB.db")
         c = conn.cursor
         c.execute('SELECT * FROM card')
         records = c.fectchall()
         conn.commit()
         conn.close()
+
+        for i in records:
+            print(i)
+
+    def decryption(self, key):
+        pass
+
+if __name__ == "__main__":
+        dao1 = DataAccessObject()
+        dao1.initializeUI()
+
+
 
