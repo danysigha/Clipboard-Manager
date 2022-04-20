c4f9b33908d614783fd36eb5b7abb590a55f393c
danysigha <nohonetdany@gmail.com>
2022-04-20 01:22:02 -0400

    Latest user class update.

--- user.py
+++ user.py
@@ -3,9 +3,12 @@ class User():
         self.userID = userID
         self._password = password
         self.currentCardID = currentCardID
+        self._saltText = None
+        self._pwdExist = False
 
     def setPassword(self, newPassword):
         self._password = newPassword
     # deleteExpiredCard should this be done in the user class?
+
     def unlock(self):
         pass #implemented by Tina
