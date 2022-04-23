c4004f4f97a55e772ffab2910d3f11c893ac6330
nko2005 <nko2005@nyu.edu>
2022-04-23 17:30:28 +0300

    changed cardlabel to cardcategory

--- FolderandCardClass/Card.py
+++ FolderandCardClass/Card.py
@@ -4,14 +4,14 @@ import uuid
 class Card:
 
 
-    def __init__(self,cardLabel,cardContent):
+    def __init__(self,cardCategory,cardContent):
 
         # "make a UUID based on the host ID and current time" .hex removes dashes and
         #turns it into a string
         self.cardID= uuid.uuid1().hex
         self.addedDate = datetime.datetime.now()
         self.modifiedDate = datetime.datetime.now()
-        self.cardLabel = cardLabel
+        self.cardCategory = cardCategory
         self.cardContent = cardContent
         self.cardFolder ="default"
         self.hideCard = False
@@ -24,17 +24,17 @@ class Card:
         return self.addedDate
     def getModifiedDate(self):
         return self.modifiedDate
-    def getCardLabel(self):
-        return self.cardLabel
+    def getCardCategory(self):
+        return self.cardCategory
     def getCardContent(self):
         return self.cardContent
     #setter functions
-    def setCardLabel(self,e):
+    def setCardCategory(self,e):
 
         if not isinstance(e,str):
             raise Exception("Card label must be str type")
 
-        self.cardLabel = e
+        self.cardCategory = e
         self.modifiedDate =datetime.datetime.now()
 
     def setCardContent(self,e):
@@ -43,7 +43,7 @@ class Card:
         self.cardContent = e
         self.modifiedDate =datetime.datetime.now()
 
-    def setcardFolder(folder):
+    def setCardFolder(folder):
         pass
 
     def setHidden(self,H):
