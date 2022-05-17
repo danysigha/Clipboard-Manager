282df3414a909593df511ffd48c53d58d416942d
nko2005 <80595010+nko2005@users.noreply.github.com>
2022-05-17 22:24:06 +0300

    added latest dao

--- dao.py
+++ dao.py
@@ -7,11 +7,11 @@ import ssl
 import string
 import random
 import clipboardManager_DB as db
-
+#Tina can help with this 
 class password_decorator:
     def __init__(self, function):
         self.function = function
-        # self.dao = DataAccessor()
+
     def password_is_valid(self, pwd):
         h = db.get_password()
         return bcrypt.checkpw(pwd.encode('utf-8'), h)
@@ -19,7 +19,7 @@ class password_decorator:
 
     def __call__(self, *args, **kwargs):
         pwd = args[0]
-        # if self.password_is_valid(pwd):
+       
         if self.password_is_valid(pwd):
             self.function(*args, **kwargs)
             result = True
@@ -29,84 +29,113 @@ class password_decorator:
         return result
 
 class DataAccessor:
-    def __init__(self):
-        pass
+    """ Data Accessor class that handles requests to the data base"""
+    
+
 # decryption takes place here
     # UI grabs the decryption key from user and pass to data access object
-    def storeCard(self, card_id, content, dataType, hideCard, favoriteCard):
-        db.addCard(1, card_id, content, dataType, hideCard, favoriteCard)
-        # card.Card(content, dataType, db.getLastCardID()+1)
+    def storeCard(self, cardId, content, dataType, hideCard, favoriteCard):
+        db.addCard(1, cardId, content, dataType, hideCard, favoriteCard)
+       
 
     def deleteCard(self, id):
-        """This method does this"""
+        """deletes card from the database
+
+        Parameters:
+        id (str): key to find the card in the database
+        
+        """
         db.deleteCard(id)
-        # db.deleteCard()
-    # def updateCard(self):
-    #     pass
-    def readCard(self, id):
-        return db.pasteCard(id)
 
     def getAllCards(self):
+        """returns all cards in the database"""
         return db.getAllCards()
 
     def getTextCards(self):
+        """returns all cards with cardCategory 'Text' """
         return db.getTextCards()
 
     def getImageCards(self):
+        """returns all cards with cardCategory 'Image' """
         return db.getImageCards()
 
     def getURLCards(self):
+         """returns all cards with cardCategory 'URL' """
         return db.getURLCards()
 
-    def hideCard(self, card_status, card_id):
+    def hideCard(self, cardStatus, cardId):
+        """ sets the hidden status of the card true or false
+        Parameters:
+        
+        cardStatus (int): desired card status, value is 1 or 0 indicating true or false respectively
+        CardId (str): key to find the card in the database
+        
+        """
+        
         db.hideCard(card_status, card_id)
 
     def getFavoriteCards(self):
+        """returns all cards from the db that are favorited"""
         return db.getFavoriteCards()
 
-    def favoriteCard(self, card_id, favorite_status):
+    def favoriteCard(self, cardId, favoriteStatus):
+        """ sets the favorite status of the card to true or false
+        Parameters:
+        
+        favoriteStatus (int): desired card status, value is 1 or 0 indicating true or false respectively
+        CardId (str): key to find the card in the database
+        
+        """
         db.favoriteCard(card_id, favorite_status)
 
     def getSearchCards(self, search):
+        """returns all cards that satisfy the search parameter
+        Parameters:
+        
+        search (str): the search query 
+
+        """
         return db.getSearchCards(search)
 
-    def changeShelftime(self, month):
-        db.changeShelftime(month)
+    def changeShelfTime(self, month):
+        """changes the shelftime to the desired month"""
+        db.changeShelfTime(month)
 
-    def get_user_status(self):
-        return db.get_user_status()
+    def getUserStatus(self):
+        """ returns the status of the user"""
+        return db.getUserStatus()
 
-    def password_is_valid(self, pwd):
-        h = db.get_password()
+    def passwordIsValid(self, pwd):
+        h = db.getPassword()
         return bcrypt.checkpw(pwd.encode('utf-8'), h)
 
     # sends encrypted password to database
     @password_decorator
-    def change_password(oldpwd, newpwd):
+    def changePassword(oldpwd, newpwd):
         salt = bcrypt.gensalt()
         hashed_pwd = bcrypt.hashpw(newpwd.encode('utf-8'), salt)
-        db.change_password(hashed_pwd)
+        db.changePassword(hashed_pwd)
 
-    def set_password(self, newpwd):
+    def setPassword(self, newpwd):
         salt = bcrypt.gensalt()
         hashed_pwd = bcrypt.hashpw(newpwd.encode('utf-8'), salt)
         db.change_password(hashed_pwd)
         self.set_password_state(1)
 
-    def set_password_state(self, state):
+    def setPasswordState(self, state):
         db.set_password_state(state)
 
-    def get_password_state(self):
-        return db.get_password_state()
+    def getPasswordState(self):
+        return db.getPasswordState()
 
-    def get_password(self):
-        return db.get_password()
+    def getPassword(self):
+        return db.getPassword()
 
-    def get_email(self):
-        return db.get_email()
+    def getEmail(self):
+        return db.getEmail()
 
-    def create_user(self, email):
-        db.create_user(email)
+    def createUser(self, email):
+        db.createUser(email)
 
     def set_email(self, email):
         db.set_email(email)
@@ -114,34 +143,21 @@ class DataAccessor:
     def resetdb(self):
         db.resetdb()
 
-    def send_email(self):
-        # conn = sqlite3.connect('ClipboardManager_DB.db')
-        # cursor = conn.cursor()
-        # records = cursor.execute("""SELECT email FROM user LIMIT 1""")
-        # email = ''
-        # for row in records:
-        #     email = row[0]
-        email = self.get_email()
+    def sendEmail(self):
+        
+        email = self.getEmail()
         port = 465  # For SSL
         smtp_server = "smtp.gmail.com"
-        sender_email = "yourclipboardmanager@gmail.com"
+        senderEmail = "yourclipboardmanager@gmail.com"
         os = platform.system()
-        receiver_email = email
-        # if os == 'Darwin':
-        #     password = "ecqibpmoeknjxwbm"
-        # elif os == 'Windows':
-        #     password = "qourwmshfkltqnnc"
+        receiverEmail = email
+       
 
         password = "ecqibpmoeknjxwbm"
         temp = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
 
-        self.set_password(temp)
-        # conn = sqlite3.connect('ClipboardManager_DB.db')
-        # cursor = conn.cursor()
-        # salt = bcrypt.gensalt()
-        # hashed_pwd = bcrypt.hashpw(temp.encode('utf-8'), salt)
-        # cursor.execute("""UPDATE user SET password = (?) WHERE userID = 1""", (hashed_pwd,))
-        # conn.commit()
+        self.setPassword(temp)
+       
 
         message = ("""\
     		Subject: Your temporary password.
@@ -149,14 +165,10 @@ class DataAccessor:
     		remember to change password to your own.""".format(temp))
 
         context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
+        with smtplib.SMTP_SSL(smtpServer, port, context=context) as server:
+            server.login(senderEmail, password)
+            server.sendmail(senderEmail, receiverEmail, message)
 
 
-if __name__ == "__main__":
-        # dao1 = DataAccessObject()
-        # dao1.initializeUI()
-        pass
 
 
