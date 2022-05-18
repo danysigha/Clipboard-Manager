da4d6743b6230bd73ab91945a5ad41a727b92b29
nko2005 <80595010+nko2005@users.noreply.github.com>
2022-05-19 00:59:20 +0300

    final card class file

--- card.py
+++ card.py
@@ -4,77 +4,90 @@ import uuid
 
 class Card:
 
-    def __init__(self, id, cardContent, cardCategory, addedDate, modifiedDate, cardFolder, hideCard=False, favoriteCard =False):
-        # "make a UUID based on the host ID and current time" .hex removes dashes and
-        # turns it into a string
-        self.cardID = id
-        self.cardContent = cardContent
-        self.cardCategory = cardCategory
-        self.addedDate = addedDate
-        self.modifiedDate = modifiedDate
-        self.cardFolder = cardFolder
+    def __init__(self, id, cardContent, cardCategory, hideCard=False, favoriteCard =False):
+        """ Creates card object with the given parameters"""
+        self._cardId = id
+        self._cardContent = cardContent
+        self._cardCategory = cardCategory
+        
         if hideCard == 0:
-            self.hideCard = False
+            self._hideCard = False
         elif hideCard == 1:
-            self.hideCard = True
+            self._hideCard = True
         else:
-            self.hideCard = hideCard
+            self._hideCard = hideCard
 
         if favoriteCard == 0:
-            self.favoriteCard = False
+            self._favoriteCard = False
         elif favoriteCard == 1:
-            self.favoriteCard = True
+            self._favoriteCard = True
         else:
-            self.favoriteCard = favoriteCard
+            self._favoriteCard = favoriteCard
 
-    # getter functions
+    
 
-    def getID(self):
-        return self.cardID
+    def getId(self):
+        """returns card Id"""
+        return self.cardId
 
     def getAddedDate(self):
+        """returns card creation date"""
         return self.addedDate
 
-    def getModifiedDate(self):
-        return self.modifiedDate
-
     def getCardCategory(self):
+        """returns the type of card content"""
         return self.cardCategory
 
     def getCardContent(self):
+        """returns the card content"""
         return self.cardContent
 
-    # setter functions
+   
     def setCardCategory(self, e):
+        
+        """
+        sets card content to passed argument
+        
+        parameters:
+            e (str): the value to set the card category to
+        """
+
+        
 
         if not isinstance(e, str):
             raise Exception("Card label must be str type")
 
         self.cardCategory = e
-        self.modifiedDate = datetime.datetime.now()
+
 
     def setCardContent(self, e):
+        """
+        sets card content to passed argument
+        
+        parameters:
+            e (str): the value to set the card content to
+        """
         if not isinstance(e, str):
             raise Exception("Card content must be str type")
         self.cardContent = e
-        self.modifiedDate = datetime.datetime.now()
 
-    def setCardFolder(folder):
-        pass
 
     def setHidden(self, H):
-        self.hideCard = H
+        """
+        sets card object hidden status
+        
+        parameters:
+            H (bool): the value to set the hideCard to
+        """
+        self._hideCard = H
 
-    # delete card
-    def __del__(self):
-        # print("card deleted")
-        # print(self)
-        pass
+   
 
     def __str__(self):
-        string = "{\n" + "card ID: " + str(self.cardID) + ", " + "\ncard content: " + "\"" + str(
-            self.cardContent) + "\"" + ", " + "\ncard type: " + str(
-            self.cardCategory) + " , " + "\ndate created: " + str(
-            self.addedDate) + " , " + "\nlast modified: " + str(self.modifiedDate) + "\nfolder ID: " + str(
-            self.cardFolder) + " , " + "\nHidden: " + str(self.hideCard) + "\n}"
+        """returns string representation for card object"""
+        
+        string = "{\n" + "card ID: " + str(self._cardId) + ", " + "\ncard content: " + "\"" + str(
+            self._cardContent) + "\"" + ", " + "\ncard type: " + str(
+            self._cardCategory) + " , " + "\ndate created: " + str(
+            self._addedDate) + " , " + "\nHidden: " + str(self._hideCard) +  "\n}"
         return string
